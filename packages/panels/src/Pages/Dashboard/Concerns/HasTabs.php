bc75cdc9cd722136dc8afdc71e2aac51f84eac03
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2024-11-22 09:01:58 +0100

    Update HasTabs.php

--- packages/panels/src/Pages/Dashboard/Concerns/HasTabs.php
+++ packages/panels/src/Pages/Dashboard/Concerns/HasTabs.php
@@ -22,20 +22,20 @@ trait HasTabs
         foreach ($this->getVisibleWidgets() as $widget) {
             $tabs[] = $this->normalizeWidgetClass($widget)::getTab();
         }
-		
-		$tabs = array_unique($tabs);
-		
+
+        $tabs = array_unique($tabs);
+
         if (in_array(null, $tabs)) {
-			if (count($tabs) === 1) {
-				// If no widget has a tab specified other than `null`, return.
-				return [];
-			}
-			
+            if (count($tabs) === 1) {
+                // If no widget has a tab specified other than `null`, return.
+                return [];
+            }
+
             $tabs = Arr::prepend($tabs, $this->getDefaultTabLabel());
         }
 
         return array_map(
-            fn (string $tab) => Tab::make($tab),
+            fn (string $tab): Tab => Tab::make($tab),
             array_unique(array_filter($tabs))
         );
     }
@@ -52,11 +52,11 @@ trait HasTabs
     {
         return array_key_first($this->getCachedTabs());
     }
-	
-	public function getDefaultTabLabel(): string
-	{
-		return __('filament-panels::pages/dashboard.tabs.default.label');
-	}
+
+    public function getDefaultTabLabel(): string
+    {
+        return __('filament-panels::pages/dashboard.tabs.default.label');
+    }
 
     public function generateTabLabel(string $key): string
     {
