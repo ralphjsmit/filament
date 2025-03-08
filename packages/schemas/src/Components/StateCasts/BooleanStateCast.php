f5eb3c249544fcecd63f62dfa6580a7355b7f421
Dan Harrin <git@danharrin.com>
2025-03-08 16:26:56 +0000

    Update BooleanStateCast.php

--- packages/schemas/src/Components/StateCasts/BooleanStateCast.php
+++ packages/schemas/src/Components/StateCasts/BooleanStateCast.php
@@ -6,13 +6,21 @@ use Filament\Schemas\Components\StateCasts\Contracts\StateCast;
 
 class BooleanStateCast implements StateCast
 {
-    public function get(mixed $state): bool
+    public function get(mixed $state): ?bool
     {
+        if (blank($state)) {
+            return null;
+        }
+
         return boolval($state);
     }
 
-    public function set(mixed $state): bool
+    public function set(mixed $state): ?bool
     {
+        if (blank($state)) {
+            return null;
+        }
+
         return boolval($state);
     }
 }
