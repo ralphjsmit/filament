f8503d917a176b79ddfe204a740cdd1e36e7668e
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2025-03-14 09:15:25 +0000

    Add configurable prefix to import and export routes

--- packages/actions/routes/web.php
+++ packages/actions/routes/web.php
@@ -4,10 +4,12 @@ use Filament\Actions\Exports\Http\Controllers\DownloadExport;
 use Filament\Actions\Imports\Http\Controllers\DownloadImportFailureCsv;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/filament/exports/{export}/download', DownloadExport::class)
+$prefix = config('filament.utility_route_prefix', 'filament');
+
+Route::get("/{$prefix}/exports/{export}/download", DownloadExport::class)
     ->name('filament.exports.download')
     ->middleware('filament.actions');
 
-Route::get('/filament/imports/{import}/failed-rows/download', DownloadImportFailureCsv::class)
+Route::get("/{$prefix}/imports/{import}/failed-rows/download", DownloadImportFailureCsv::class)
     ->name('filament.imports.failed-rows.download')
     ->middleware('filament.actions');
