2cbb9a5a3dad53fe98f28ffd3ca6029983be313f
Leandro Ferreira <leandrocfe@gmail.com>
2025-03-25 08:36:38 -0300

    update

--- packages/schemas/src/Schema.php
+++ packages/schemas/src/Schema.php
@@ -260,7 +260,7 @@ class Schema extends ViewComponent implements HasEmbeddedView
                                     )) ?>"
                                 <?php } ?>
                                 <?php if (filled($xShow = match ([filled($hiddenJs), filled($visibleJs)]) {
-                                    [true, true] => "(! {$hiddenJs}) && ({$visibleJs})",
+                                    [true, true] => "(! ({$hiddenJs})) && ({$visibleJs})",
                                     [true, false] => "! ({$hiddenJs})",
                                     [false, true] => $visibleJs,
                                     default => null,
