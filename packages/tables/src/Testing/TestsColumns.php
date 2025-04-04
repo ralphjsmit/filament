44f0a73ee979aa28d28ae2bd7f50f06c7ca5f4c5
Mrkbingham <Mrkbingham@users.noreply.github.com>
2025-04-04 20:18:40 +0000

    chore: fix code style

--- packages/tables/src/Testing/TestsColumns.php
+++ packages/tables/src/Testing/TestsColumns.php
@@ -286,7 +286,7 @@ class TestsColumns
             $livewireClass = $this->instance()::class;
 
             $column->clearCache();
-            
+
             Assert::assertFalse(
                 $column->formatState($column->getState()) == $value,
                 message: "Failed asserting that a table column with name [{$name}] does not have a formatted state of [{$value}] for record [{$record->getKey()}] on the [{$livewireClass}] component.",
