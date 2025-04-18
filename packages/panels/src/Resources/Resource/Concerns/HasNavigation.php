e3e416a25dabab5a799b4911aa82e7884a9da4ff
Dan Harrin <git@danharrin.com>
2025-04-18 16:37:47 +0100

    clean up parent resource registration

--- packages/panels/src/Resources/Resource/Concerns/HasNavigation.php
+++ packages/panels/src/Resources/Resource/Concerns/HasNavigation.php
@@ -43,7 +43,7 @@ trait HasNavigation
             return;
         }
 
-        if (static::getParentResource()) {
+        if (static::getParentResourceRegistration()) {
             return;
         }
 
