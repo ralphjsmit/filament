1b1eaedbfd05cca7ccaf61962845b8420d1683d9
Dan Harrin <git@danharrin.com>
2025-05-08 12:26:30 +0100

    Upgrade TipTap to v3 beta 5

--- packages/forms/resources/js/components/rich-editor/extensions.js
+++ packages/forms/resources/js/components/rich-editor/extensions.js
@@ -1,17 +1,15 @@
+import { UndoRedo } from '@tiptap/extensions'
 import Blockquote from '@tiptap/extension-blockquote'
 import Bold from '@tiptap/extension-bold'
-import BulletList from '@tiptap/extension-bullet-list'
 import Code from '@tiptap/extension-code'
 import CodeBlock from '@tiptap/extension-code-block'
 import Document from '@tiptap/extension-document'
 import Heading from '@tiptap/extension-heading'
-import History from '@tiptap/extension-history'
 import Italic from '@tiptap/extension-italic'
 import Image from './extension-image.js'
 import Link from '@tiptap/extension-link'
-import ListItem from '@tiptap/extension-list-item'
+import { BulletList, ListItem, OrderedList } from '@tiptap/extension-list'
 import LocalFiles from './extension-local-files.js'
-import OrderedList from '@tiptap/extension-ordered-list'
 import Paragraph from '@tiptap/extension-paragraph'
 import Strike from '@tiptap/extension-strike'
 import Subscript from '@tiptap/extension-subscript'
@@ -27,7 +25,6 @@ export default ({ key, statePath, uploadingFileMessage, $wire }) => [
     CodeBlock,
     Document,
     Heading,
-    History,
     Italic,
     Image,
     Link.configure({
@@ -48,4 +45,5 @@ export default ({ key, statePath, uploadingFileMessage, $wire }) => [
     Superscript,
     Text,
     Underline,
+    UndoRedo,
 ]
