cd757b74cb2573a77b956003037c21f7926851f0
Dan Harrin <git@danharrin.com>
2025-06-10 14:40:27 +0100

    Update header-cell.blade.php

--- packages/tables/resources/views/components/header-cell.blade.php
+++ packages/tables/resources/views/components/header-cell.blade.php
@@ -29,8 +29,8 @@
             role="button"
             tabindex="0"
             wire:click="sortTable('{{ $name }}')"
-            x-on:keydown.enter="$wire.sortTable('{{ $name }}')"
-            x-on:keydown.space.prevent="$wire.sortTable('{{ $name }}')"
+            x-on:keydown.enter.prevent.stop="$wire.sortTable('{{ $name }}')"
+            x-on:keydown.space.prevent.stop="$wire.sortTable('{{ $name }}')"
         @endif
         @class([
             'group flex w-full items-center gap-x-1',
