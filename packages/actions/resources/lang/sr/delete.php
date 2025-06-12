95f5e765f85344b0f0bdf9ec229aefe2ff608949
Nikola Radovanovic <nikola.r@toptal.com>
2025-06-13 00:36:46 +0200

    Serbian translation

---
+++ packages/actions/resources/lang/sr/delete.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Избриши',
+
+        'modal' => [
+
+            'heading' => 'Избриши :label',
+
+            'actions' => [
+
+                'delete' => [
+                    'label' => 'Избриши',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'deleted' => [
+                'title' => 'Избрисано',
+            ],
+
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Избриши изабрано',
+
+        'modal' => [
+
+            'heading' => 'Избриши изабрано',
+
+            'actions' => [
+
+                'delete' => [
+                    'label' => 'Избриши',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'deleted' => [
+                'title' => 'Избрисано',
+            ],
+
+        ],
+
+    ],
+
+];
---
+++ packages/actions/resources/lang/sr/restore.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Врати',
+
+        'modal' => [
+
+            'heading' => 'Врати :label',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'Врати',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'restored' => [
+                'title' => 'Враћено',
+            ],
+
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Врати изабрано',
+
+        'modal' => [
+
+            'heading' => 'Врати изабрано :label',
+
+            'actions' => [
+
+                'restore' => [
+                    'label' => 'Врати',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'restored' => [
+                'title' => 'Враћено',
+            ],
+
+        ],
+
+    ],
+
+];
