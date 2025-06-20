92be83a4dfcef49bffcf3f4a1a7265dd09217d84
Sviat <s.fartushnyi@gmail.com>
2025-06-20 21:01:48 +0300

    Update delete.php

--- packages/actions/resources/lang/uk/delete.php
+++ packages/actions/resources/lang/uk/delete.php
@@ -56,14 +56,14 @@ return [
 
             'deleted_partial' => [
                 'title' => 'Видалено :count із :total',
-                'missing_authorization_failure_message' => 'У вас немає дозволу на видалення :count.',
-                'missing_processing_failure_message' => 'Не вдалося видалити :count.',
+                'missing_authorization_failure_message' => 'У вас немає дозволу на видалення :count  записів.',
+                'missing_processing_failure_message' => 'Не вдалося видалити :count  записів.',
             ],
 
             'deleted_none' => [
                 'title' => 'Не вдалося видалити',
-                'missing_authorization_failure_message' => 'У вас немає дозволу на видалення :count.',
-                'missing_processing_failure_message' => 'Не вдалося видалити :count.',
+                'missing_authorization_failure_message' => 'У вас немає дозволу на видалення :count  записів.',
+                'missing_processing_failure_message' => 'Не вдалося видалити :count  записів.',
             ],
 
 
