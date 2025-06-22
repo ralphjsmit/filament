64e7b204f2391d04086efc463588c47778f6aa9c
Dan Harrin <git@danharrin.com>
2025-06-22 12:21:18 +0100

    fix: Select dropdown positioning

--- packages/forms/resources/js/components/select.js
+++ packages/forms/resources/js/components/select.js
@@ -1274,20 +1274,32 @@ class CustomSelect {
     }
 
     async openDropdown() {
-        // Make dropdown visible but with position absolute and opacity 0 for measurement
+        // Make dropdown visible but with position fixed and opacity 0 for measurement
         this.dropdown.style.display = 'block'
         this.dropdown.style.opacity = '0'
-        this.dropdown.style.position = 'absolute'
+        this.dropdown.style.position = 'fixed'
+        // Set width immediately to match the select button
+        this.dropdown.style.width = `${this.selectButton.offsetWidth}px`
         this.selectButton.setAttribute('aria-expanded', 'true')
         this.isOpen = true
 
         // Position the dropdown using Floating UI
         this.positionDropdown()
 
-        // Add resize listener to update position when window is resized
+        // Add resize listener to update width and position when window is resized
         if (!this.resizeListener) {
-            this.resizeListener = () => this.positionDropdown()
+            this.resizeListener = () => {
+                // Update width to match the select button
+                this.dropdown.style.width = `${this.selectButton.offsetWidth}px`
+                this.positionDropdown()
+            }
             window.addEventListener('resize', this.resizeListener)
+        }
+
+        // Add scroll listener to update position when page is scrolled
+        if (!this.scrollListener) {
+            this.scrollListener = () => this.positionDropdown()
+            window.addEventListener('scroll', this.scrollListener, true)
         }
 
         // Make dropdown visible
@@ -1389,11 +1401,11 @@ class CustomSelect {
         computePosition(this.selectButton, this.dropdown, {
             placement: placement,
             middleware: middleware,
+            strategy: 'fixed',
         }).then(({ x, y }) => {
             Object.assign(this.dropdown.style, {
                 left: `${x}px`,
                 top: `${y}px`,
-                width: `${this.selectButton.offsetWidth}px`,
             })
         })
     }
@@ -1407,6 +1419,12 @@ class CustomSelect {
         if (this.resizeListener) {
             window.removeEventListener('resize', this.resizeListener)
             this.resizeListener = null
+        }
+
+        // Remove scroll listener
+        if (this.scrollListener) {
+            window.removeEventListener('scroll', this.scrollListener, true)
+            this.scrollListener = null
         }
 
         // Remove focus from all options
@@ -2047,6 +2065,12 @@ class CustomSelect {
         if (this.resizeListener) {
             window.removeEventListener('resize', this.resizeListener)
             this.resizeListener = null
+        }
+
+        // Remove scroll event listener if it exists
+        if (this.scrollListener) {
+            window.removeEventListener('scroll', this.scrollListener, true)
+            this.scrollListener = null
         }
 
         // Remove the event listener for refreshing selected option labels if it was added
