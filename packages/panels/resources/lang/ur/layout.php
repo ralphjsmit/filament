35b8cd5a8853f20637551250657bd1e66fd88666
hammadarain05 <hammadarain05@gmail.com>
2025-06-29 13:19:35 +0000

    urdu translations added for layout.php

---
+++ packages/panels/resources/lang/ur/layout.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+    'direction' => 'rtl',
+
+    'actions' => [
+
+        'billing' => [
+            'label' => 'سبسکرپشن کا انتظام کریں',
+        ],
+
+        'logout' => [
+            'label' => 'سائن آؤٹ',
+        ],
+
+        'open_database_notifications' => [
+            'label' => 'اطلاعات کھولیں',
+        ],
+
+        'open_user_menu' => [
+            'label' => 'یوزر مینو',
+        ],
+
+        'sidebar' => [
+
+            'collapse' => [
+                'label' => 'سائڈبار کو سمیٹیں',
+            ],
+
+            'expand' => [
+                'label' => 'سائڈبار کو پھیلائیں',
+            ],
+
+        ],
+
+        'theme_switcher' => [
+
+            'dark' => [
+                'label' => 'ڈارک تھیم فعال کریں',
+            ],
+
+            'light' => [
+                'label' => 'لائٹ تھیم فعال کریں',
+            ],
+
+            'system' => [
+                'label' => 'سسٹم تھیم فعال کریں',
+            ],
+
+        ],
+
+    ],
+
+    'avatar' => [
+        'alt' => ':name کا اوتار',
+    ],
+
+    'logo' => [
+        'alt' => ':name کا لوگو',
+    ],
+
+];
