db1f53a9e43652b16a04510ab9906a4a5950fbad
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2025-07-02 17:22:59 +0200

    Update test-component-with-form.blade.php

--- tests/resources/views/forms/test-component-with-form.blade.php
+++ tests/resources/views/forms/test-component-with-form.blade.php
@@ -1,10 +1,10 @@
 <x-dynamic-component
-		:component="$getFieldWrapperView()"
-		:field="$field"
+	:component="$getFieldWrapperView()"
+	:field="$field"
 >
 	<div
-			x-data="{ state: $wire.$entangle(@js($getStatePath())) }"
-			{{ $getExtraAttributeBag() }}
+		x-data="{ state: $wire.$entangle(@js($getStatePath())) }"
+		{{ $getExtraAttributeBag() }}
 	>
 		{{ $getChildComponentContainer() }}
 	</div>
