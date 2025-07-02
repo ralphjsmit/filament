20abaa8760ecdd3c763b98a0856f7994253efcfb
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2025-07-02 17:32:36 +0200

    WIP

--- tests/src/Forms/HintTest.php
+++ tests/src/Forms/HintTest.php
@@ -1,10 +1,7 @@
 <?php
 
-use Filament\Actions\Action;
 use Filament\Forms\Components\Field;
 use Filament\Forms\Components\TextInput;
-use Filament\Schemas\Components\Section;
-use Filament\Schemas\Components\Wizard;
 use Filament\Schemas\Schema;
 use Filament\Tests\Fixtures\Livewire\Livewire;
 use Filament\Tests\TestCase;
@@ -14,13 +11,14 @@ use function Filament\Tests\livewire;
 uses(TestCase::class);
 
 it('has a form with the default name \'form\'', function (): void {
-    livewire(TestComponentWithForm::class)
+    livewire(TestComponentWithFormWithTestFieldHint::class)
         ->assertFormExists();
 });
 
 class TestFieldWithChildComponentSchema extends Field
 {
-	protected string $view = 'forms.test-component-with-form';
+    protected string $view = 'forms.test-component-with-form';
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -42,33 +40,33 @@ class TestFieldWithChildComponentSchema extends Field
 
                 $component->state($state);
             })
-	        ->mutateDehydratedStateUsing(function (array $state): ?string {
-				if (! $state['partA'] || ! $state['partB']) {
-					return null;
-				}
+            ->mutateDehydratedStateUsing(function (array $state): ?string {
+                if (! $state['partA'] || ! $state['partB']) {
+                    return null;
+                }
 
-				return $state['partA'] . '-' . $state['partB'];
-			})
-	        ->schema([
-				TextInput::make('partA'),
-				TextInput::make('partB'),
-	        ])
-	        ->hint(function (array $state) {
-				// Must be an array as the `afterStateHydrated()` ensures it is always an array.
-				return "{$state['partA']} / {$state['partB']}";
-	        });
+                return $state['partA'] . '-' . $state['partB'];
+            })
+            ->schema([
+                TextInput::make('partA'),
+                TextInput::make('partB'),
+            ])
+            ->hint(function (array $state) {
+                // Must be an array as the `afterStateHydrated()` ensures it is always an array.
+                return "{$state['partA']} / {$state['partB']}";
+            });
     }
 }
 
-class TestComponentWithForm extends Livewire
+class TestComponentWithFormWithTestFieldHint extends Livewire
 {
-	public function mount(): void
-	{
-		$this->form->fill([
-			'schema' => 'a-b'
-		]);
-	}
-	
+    public function mount(): void
+    {
+        $this->form->fill([
+            'schema' => 'a-b',
+        ]);
+    }
+
     public function form(Schema $form): Schema
     {
         return $form
