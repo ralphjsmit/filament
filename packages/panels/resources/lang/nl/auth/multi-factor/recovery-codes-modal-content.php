9f002d09d64774a1eecdf6c1b00980534081b9c8
MarcelWeidum <marcelweidum@live.nl>
2025-07-17 16:59:07 +0200

    Add dutch 2FA translations

---
+++ packages/panels/resources/lang/nl/auth/multi-factor/recovery-codes-modal-content.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'actions' => [
+
+        'Klik om',
+
+        'copy' => [
+            'label' => 'kopiëren',
+        ],
+
+        'of',
+
+        'download' => [
+            'label' => 'download',
+        ],
+
+        'alle codes in één keer.',
+
+    ],
+
+    'messages' => [
+        'copied' => 'Gekopieerd',
+    ],
+
+];
