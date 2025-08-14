543081befbc7ea45826ac8c28667e6b0c13ff245
Dan Harrin <git@danharrin.com>
2025-08-14 10:04:59 +0100

    Update user-menu.blade.php

--- packages/panels/resources/views/components/user-menu.blade.php
+++ packages/panels/resources/views/components/user-menu.blade.php
@@ -51,9 +51,11 @@
         @endphp
 
         {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::USER_MENU_PROFILE_BEFORE) }}
+
         <x-filament::dropdown.header :color="$itemColor" :icon="$itemIcon">
             {{ $item->getLabel() }}
         </x-filament::dropdown.header>
+
         {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::USER_MENU_PROFILE_AFTER) }}
     @endif
 
